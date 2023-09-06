74a034e21d4295f62d4f79cbdd8bbb51f529a9b1
Marquisse <mareck1@wp.pl>
2023-09-06 14:43:32 +0200

    Zadanie domowe dzień 5

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,18 +1,66 @@
-﻿int[] grades = new int[365];
-string[] dayOfWeeks = new string[7];
-dayOfWeeks[0] = "poniedziałek";
-dayOfWeeks[1] = "wtorek";
-dayOfWeeks[2] = "środa";
-dayOfWeeks[3] = "czwartek";
-dayOfWeeks[4] = "piątek";
-dayOfWeeks[5] = "sobota";
-dayOfWeeks[6] = "niedziela";
-Console.WriteLine(dayOfWeeks[2]);
-string[] dayOfWeeks2 = {"poniedziałek", "wtorek", "środa", "czwartek", "piątek", "sobota", "niedziela" };
-Console.WriteLine(dayOfWeeks2[0]);
+﻿//Zadanie domowe dzień 5
+using System;
 
-for(int i = 0; i<10; i = i + 1)
+int number = 255478;
+string numberInString = number.ToString();
+char[] letters = numberInString.ToArray();
+//int[] result = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+int[] result = new int[10];
+
+Console.WriteLine("Wyniki dla liczby: " + number);
+foreach (char letter in letters)
 {
-    Console.WriteLine(i);
+    if (letter == '0')
+    {
+        result[0] = result[0] + 1;
+    }
+    else if (letter == '1')
+    {
+        result[1] = result[1] + 1;
+    }
+    else if (letter == '2')
+    {
+        result[2] = result[2] + 1;
+    }
+    else if (letter == '3')
+    {
+        result[3] = result[3] + 1;
+    }
+    else if (letter == '4')
+    {
+        result[4] = result[4] + 1;
+    }
+    else if (letter == '5')
+    {
+        result[5] = result[5] + 1;
+    }
+    else if (letter == '6')
+    {
+        result[6] = result[6] + 1;
+    }
+    else if (letter == '7')
+    {
+        result[7] = result[7] + 1;
+    }
+    else if (letter == '8')
+    {
+        result[8] = result[8] + 1;
+    }
+    else if (letter == '9')
+    {
+        result[9] = result[9] + 1;
+    }
 }
+
+Console.WriteLine("0 => " + result[0]);
+Console.WriteLine("1 => " + result[1]);
+Console.WriteLine("2 => " + result[2]);
+Console.WriteLine("3 => " + result[3]);
+Console.WriteLine("4 => " + result[4]);
+Console.WriteLine("5 => " + result[5]);
+Console.WriteLine("6 => " + result[6]);
+Console.WriteLine("7 => " + result[7]);
+Console.WriteLine("8 => " + result[8]);
+Console.WriteLine("9 => " + result[9]);
+
 Console.ReadLine();
