59cd7277f898fc6f780439d981561451ebbe6566
Marquisse <mareck1@wp.pl>
2023-09-07 16:47:22 +0200

    Dzień 6 ćwiczenie z klasami i wieloma konstruktorami

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,65 +1,39 @@
-﻿//Zadanie domowe dzień 5
+﻿User user1 = new User();
+User user2 = new User("Zuzia");
+User user3 = new User("Marek", "hasło");
+User user4 = new User();
 
-int number = 255478;
-string numberInString = number.ToString();
-char[] letters = numberInString.ToArray();
-//int[] result = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-int[] result = new int[10];
-
-Console.WriteLine("Wyniki dla liczby: " + number);
-foreach (char letter in letters)
+class User
 {
-    if (letter == '0')
+    private string login;
+    private string password;
+    private string name;
+
+    public User()
     {
-        result[0] = result[0] + 1;
+        this.login = "-";
+        this.password = "-";
+        this.name = "-";
     }
-    else if (letter == '1')
+
+    public User(string login)
     {
-        result[1] = result[1] + 1;
+        this.login = login;
+        this.password = "-";
+        this.name = "-";
     }
-    else if (letter == '2')
+
+    public User(string login, string password)
     {
-        result[2] = result[2] + 1;
+        this.login = login;
+        this.password = password;
+        this.name = "-";
     }
-    else if (letter == '3')
+    public User(string login, string password, string name)
     {
-        result[3] = result[3] + 1;
+        this.login = login;
+        this.password = password;
+        this.name = name;
     }
-    else if (letter == '4')
-    {
-        result[4] = result[4] + 1;
-    }
-    else if (letter == '5')
-    {
-        result[5] = result[5] + 1;
-    }
-    else if (letter == '6')
-    {
-        result[6] = result[6] + 1;
-    }
-    else if (letter == '7')
-    {
-        result[7] = result[7] + 1;
-    }
-    else if (letter == '8')
-    {
-        result[8] = result[8] + 1;
-    }
-    else if (letter == '9')
-    {
-        result[9] = result[9] + 1;
-    }
+
 }
-
-Console.WriteLine("0 => " + result[0]);
-Console.WriteLine("1 => " + result[1]);
-Console.WriteLine("2 => " + result[2]);
-Console.WriteLine("3 => " + result[3]);
-Console.WriteLine("4 => " + result[4]);
-Console.WriteLine("5 => " + result[5]);
-Console.WriteLine("6 => " + result[6]);
-Console.WriteLine("7 => " + result[7]);
-Console.WriteLine("8 => " + result[8]);
-Console.WriteLine("9 => " + result[9]);
-
-Console.ReadLine();
