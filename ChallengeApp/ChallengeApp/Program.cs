a8dd1776ae4d48e2e3dbae7d6a11bdaf29fe41e3
Marquisse <mareck1@wp.pl>
2023-09-12 14:34:24 +0200

    Dzień 6 ćwiczenie - Metody

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -3,16 +3,24 @@ User user2 = new User("Zuzia", "123456");
 User user3 = new User("Marek", "hasło");
 User user4 = new User("Stefan", "123456");
 
-var name = user1.Login;
-Console.WriteLine(name);
+user1.AddScore(5);
+user1.AddScore(7);
+user1.AddScore(2);
+Console.WriteLine(user1.score);
 class User
 {
+    public int score { get; private set; }
     public User(string login, string password)
     {
         this.Login = login;
         this.Password = password;
+        this.score = 0;
     }
     public string Login { get; private set; }
     public string Password { get; set; }
 
+    public void AddScore(int number)
+    {
+        this.score += number;
+    }
 }
