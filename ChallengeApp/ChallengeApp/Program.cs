cd60e53897a65b693056b5db63fa237f782ea2d7
Marquisse <mareck1@wp.pl>
2023-10-06 01:08:05 +0200

    Dzień 6 - zadanie domowe

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,12 +1,46 @@
 ﻿using ChallengeApp;
 
-User user1 = new User("Adam", "123456");
-User user2 = new User("Zuzia", "123456");
-User user3 = new User("Marek", "hasło");
-User user4 = new User("Stefan", "123456");
+Employee employee1 = new Employee("Adam", "Adamski", 25);
+Employee employee2 = new Employee("Bartek", "Bartkowski", 30);
+Employee employee3 = new Employee("Cyprian", "Cypriański", 35);
 
-user1.AddScore(5);
-user1.AddScore(7);
-user1.AddScore(2);
-var result = user1.Result;
-Console.WriteLine(result);
+//Task 1
+employee1.AddPoints(5);
+employee2.AddPoints(6);
+employee3.AddPoints(7);
+//Task  2
+employee1.AddPoints(4);
+employee2.AddPoints(9);
+employee3.AddPoints(3);
+//Task 3
+employee1.AddPoints(7);
+employee2.AddPoints(1);
+employee3.AddPoints(8);
+//Task  4
+employee1.AddPoints(5);
+employee2.AddPoints(6);
+employee3.AddPoints(2);
+//Task  5
+employee1.AddPoints(3);
+employee2.AddPoints(5);
+employee3.AddPoints(5);
+
+var employee1_score = employee1.Score;
+var employee2_score = employee2.Score;
+var employee3_score = employee3.Score;
+
+if (employee1_score > employee2_score && employee1_score > employee3_score)
+{
+    int highest_score = employee1_score;
+    Console.WriteLine("Najwyższą sumę ocen uzyskał " + employee1.Name + " " + employee1.Surname + " (lat " + employee1.Age + ") i wynosi ona " + highest_score + ".");
+}
+else if (employee2_score > employee3_score)
+{
+    int highest_score = employee2_score;
+    Console.WriteLine("Najwyższą sumę ocen uzyskał " + employee2.Name + " " + employee2.Surname + " (lat " + employee2.Age + ") i wynosi ona " + highest_score + ".");
+}
+else if (employee3_score > employee2_score)
+{
+    int highest_score = employee3_score;
+    Console.WriteLine("Najwyższą sumę ocen uzyskał " + employee3.Name + " " + employee3.Surname + " (lat " + employee3.Age +") i wynosi ona " + highest_score + ".");
+}
