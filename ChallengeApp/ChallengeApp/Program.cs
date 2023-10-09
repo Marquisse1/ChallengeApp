0314f2260d386b451602c3d0fa4fd4bcc8ebca5b
Marquisse <mareck1@wp.pl>
2023-10-09 18:17:07 +0200

    Usunąłem niepotrzebne zmienne

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -8,7 +8,7 @@ Employee employee3 = new Employee("Cyprian", "Cypriański", 35);
 employee1.AddPoints(5);
 employee2.AddPoints(6);
 employee3.AddPoints(7);
-//Task  2
+//Task 2
 employee1.AddPoints(4);
 employee2.AddPoints(9);
 employee3.AddPoints(3);
@@ -16,7 +16,7 @@ employee3.AddPoints(3);
 employee1.AddPoints(7);
 employee2.AddPoints(1);
 employee3.AddPoints(8);
-//Task  4
+//Task 4
 employee1.AddPoints(5);
 employee2.AddPoints(6);
 employee3.AddPoints(2);
@@ -25,22 +25,22 @@ employee1.AddPoints(3);
 employee2.AddPoints(5);
 employee3.AddPoints(5);
 
-var employee1_score = employee1.Score;
-var employee2_score = employee2.Score;
-var employee3_score = employee3.Score;
+//var employee1_score = employee1.Score;
+//var employee2_score = employee2.Score;
+//var employee3_score = employee3.Score;
 
-if (employee1_score > employee2_score && employee1_score > employee3_score)
+if (employee1.Score > employee2.Score && employee1.Score > employee3.Score)
 {
-    int highest_score = employee1_score;
-    Console.WriteLine("Najwyższą sumę ocen uzyskał " + employee1.Name + " " + employee1.Surname + " (lat " + employee1.Age + ") i wynosi ona " + highest_score + ".");
+    //int highest_score = employee1_score;
+    Console.WriteLine("Najwyższą sumę ocen uzyskał " + employee1.Name + " " + employee1.Surname + " (lat " + employee1.Age + ") i wynosi ona " + employee1.Score + ".");
 }
-else if (employee2_score > employee3_score)
+else if (employee2.Score > employee3.Score)
 {
-    int highest_score = employee2_score;
-    Console.WriteLine("Najwyższą sumę ocen uzyskał " + employee2.Name + " " + employee2.Surname + " (lat " + employee2.Age + ") i wynosi ona " + highest_score + ".");
+    //int highest_score = employee2_score;
+    Console.WriteLine("Najwyższą sumę ocen uzyskał " + employee2.Name + " " + employee2.Surname + " (lat " + employee2.Age + ") i wynosi ona " + employee2.Score + ".");
 }
-else if (employee3_score > employee2_score)
+else if (employee3.Score > employee2.Score)
 {
-    int highest_score = employee3_score;
-    Console.WriteLine("Najwyższą sumę ocen uzyskał " + employee3.Name + " " + employee3.Surname + " (lat " + employee3.Age +") i wynosi ona " + highest_score + ".");
+    //int highest_score = employee3_score;
+    Console.WriteLine("Najwyższą sumę ocen uzyskał " + employee3.Name + " " + employee3.Surname + " (lat " + employee3.Age + ") i wynosi ona " + employee3.Score + ".");
 }
